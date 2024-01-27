215218884118547bd80178ae50061087e2376745
BenZiAv <benavnilov@gmail.com>
2024-01-27 14:37:53 +0200

    fix isuue
    homepageheader name ch
    contact reorder

--- frontend/src/components/contact_us/Contact.js
+++ frontend/src/components/contact_us/Contact.js
@@ -8,11 +8,11 @@ const Contact = ({ activePage }) => {
         Email : <a href="mailto:benavnilov@gmail.com">benavnilov@gmail.com</a>
       </h4>
       <h4>
-        Email : <a href="mailto:y.s.aldin@gmail.com">y.s.aldin@gmail.com</a>
+        Phone : <a href="tel:+972507744439">+972507744439</a>
       </h4>
 
       <h4>
-        Phone : <a href="tel:+972507744439">+972507744439</a>
+        Email : <a href="mailto:y.s.aldin@gmail.com">y.s.aldin@gmail.com</a>
       </h4>
       <h4>
         Phone : <a href="tel:+972525327377">+972525327377</a>
